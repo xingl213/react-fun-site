dab85b1a9e1ab97ff2e8adc311876415b3018478
xingl213 <hylxjackson@gmail.com>
2022-01-05 22:45:57 -0500

    used HashRouter for github pages deployment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Main from './components/MainComponent';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Main />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
